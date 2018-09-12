72c32f840e6549295f43a587af8ea797c8c31c97
lopespt <guilhermewachs@gmail.com>
2018-09-12 15:22:22 -0300

    removido reacjs default forum

--- Aplicacao.js
+++ Aplicacao.js
@@ -6,7 +6,7 @@ export default class Aplicacao extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            forum: "reactjs"
+            forum: ""
         }
 
     }
